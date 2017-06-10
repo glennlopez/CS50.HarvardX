bddb53906ec1f62e3b1d9a5ce0a013875a5813c5
Glenn Lopez <glennlopez@me.com>
2017-06-10 04:56:41 +0000

    solved mario more

--- pset1/mario.c
+++ pset1/mario.c
@@ -2,21 +2,41 @@
 #include<cs50.h>
 
 int get_posInt(void);
+void printPyramid(int param);
 
 int main(){
     
-    unsigned int usrNum = get_posInt();
-    printf("Num: %i\n", usrNum);    //print debug
+    int usrNum = get_posInt();
+    printPyramid(usrNum);
     
     return 0;
 }
 
 
 //check get_int() for positive number
-int get_posInt(void){int posInt = -999999; 
+int get_posInt(void){int posInt = -9; 
     do{
+        printf("Height: ");
         posInt = get_int();
     }while( (posInt < 0) || (posInt >= 24) );
     
     return posInt;
+}
+
+
+//print out pyramid
+void printPyramid(int param){
+    for(int i = 0; i < param; i++){
+        for(int j = param; j > i+1; j--){
+            printf(" ");
+        }
+        for(int j = 0; j <= i; j++){
+            printf("#");
+        }
+        printf("  ");
+        for(int j = 0; j <= i; j++){
+            printf("#");
+        }
+        printf("\n");
+    }
 }
