beff4487aa3fb64abb04839f8ad3819102794a46
Glenn Lopez <glennlopez@me.com>
2017-06-10 23:14:32 +0000

    update

--- pset1/credit.c
+++ pset1/credit.c
@@ -6,8 +6,7 @@
 //prototypes
 long long get_posLongLong(void);
 int countDigits(long long param);
-void checkType(long long param);
-
+void checkType(int param[], int);
 
 
 
@@ -21,19 +20,13 @@ int main(){
     
     //store each card digit inside array ccNum[...]
     unsigned long long nextCardDigit = 1;
-    for(int i = 0; i < countDigits(cc_number); i++){
-        printf("Credit Card(%i): %lld\n", digitCount, cc_number);  //debug - digit count
+    for(int i = countDigits(cc_number) -1; i >= 0; i--){
         ccNum[i] = ((cc_number%(10 * nextCardDigit))/(1 * nextCardDigit));
-        
         nextCardDigit *= 10;    //iterate through the next digit
     }
     
-    //debug - display array contents
-    for(int i = countDigits(cc_number) -1; i >= 0; i--){
-        printf("%i ", ccNum[i]);
-    }
-
-    printf("\n");
+    checkType(ccNum, digitCount);
+    
     return 0;
 }
 
@@ -52,19 +45,130 @@ long long get_posLongLong(void){long long posInt = -9;
     return posInt;
 }
 
+
+//check the type of cc used
+void checkType(int ccNum[], int digitCount){
+    
+    int checksum = -9999;
+    int sumOfProduct = 0; //sum of digits multiplied by 2
+    int sumOfdigits = 0; //sum of digits NOT multiplied by 2
+    int n = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //CHECKSUM ROUTINE 
+    for(int i = 0; i < (digitCount/2); i++){
+        if((ccNum[1 + n] * 2) >= 10){
+            sumOfProduct += ((ccNum[1 + n] * 2)%10);
+            sumOfProduct += ((ccNum[1 + n] * 2)/10);
+        }
+        else{
+            sumOfProduct += (ccNum[1 + n] * 2);
+        }
+        n += 2;
+    }
+
+    printf("sumOfProduct: %i\n", sumOfProduct);
+    
+    
+    
+    
+    
+    //MASTERCARD - 16 digits (51,52,53,54,55)
+    //AMEX - 15 digits (34,37)
+    //VISA - 13 and 16 digits (4)
+    
+    
+    
+    
+    
+    
+    
+
+    n = 0;
+    for(int i = 0; i < (digitCount/2) ; i++){
+        sumOfdigits += (ccNum[0 + n]);
+        n += 2;
+    }
+    printf("sumOfdigits: %i\n", sumOfdigits);
+    
+    
+    
+    
+    // CARD TYPE CHECK
+    checksum = (sumOfProduct + sumOfdigits) % 10;
+    //printf("checksum: %i\n", checksum);
+    if(checksum == 0){
+        //VISA - 13 and 16 digits (4)
+        if( (digitCount == 13) || (digitCount == 16) ){
+            if(ccNum[0] == 4){
+                printf("VISA\n");
+            }    
+        }//END VISA CHECK
+        
+        //MASTERCARD - 16 digits (51,52,53,54,55)
+        if(digitCount == 16){
+            if(ccNum[0] == 5){
+                if( 
+                    (ccNum[1] == 1) || 
+                    (ccNum[1] == 2) || 
+                    (ccNum[1] == 3) || 
+                    (ccNum[1] == 4) || 
+                    (ccNum[1] == 5) 
+                ){
+                    printf("MASTERCARD\n");
+                }
+            }
+            
+        }//END MASTERCARD CHECK
+        
+        //AMEX - 15 digits (34,37)
+        if(digitCount == 15){
+            if(ccNum[0] == 3){
+                if( (ccNum[1] == 4) || (ccNum[1] == 7) ){
+                    printf("AMEX\n");
+                }
+            }
+        } //END AMEX CHECK
+
+    }
+    else{
+        printf("INVALID\n");
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
+
 //check the number of digits in cc_number
 int countDigits(long long param){
-    if (param < 10) return 1;               // INVALID 
-    if (param < 100) return 2;              // INVALID
-    if (param < 1000) return 3;             // INVALID
-    if (param < 10000) return 4;            // INVALID
-    if (param < 100000) return 5;           // INVALID
-    if (param < 1000000) return 6;          // INVALID
-    if (param < 10000000) return 7;         // INVALID
-    if (param < 100000000) return 8;        // INVALID
-    if (param < 1000000000) return 9;       // INVALID
-    if (param < 10000000000) return 10;     // INVALID
-    if (param < 100000000000) return 11;    // Dankort (PBS)
+    if (param < 10) return 1;               
+    if (param < 100) return 2;             
+    if (param < 1000) return 3;            
+    if (param < 10000) return 4;           
+    if (param < 100000) return 5;          
+    if (param < 1000000) return 6;         
+    if (param < 10000000) return 7;        
+    if (param < 100000000) return 8;       
+    if (param < 1000000000) return 9;      
+    if (param < 10000000000) return 10;    
+    if (param < 100000000000) return 11;    
     // minimum length on a cc is 12 digits
     if (param < 1000000000000) return 12;
     if (param < 10000000000000) return 13;
@@ -75,16 +179,4 @@ int countDigits(long long param){
     if (param < 1000000000000000000) return 18;
     // maxumum length on a cc & long long is 19 digits
     return 19;
-}
-
-//check the type of cc used
-void checkType(long long param){
-    printf("FIXME: %lld\n", param);
-    //CHECKSUM INVALID 
-    
-    //VISA - 
-    
-    //MASTERCARD - 
-    
-    //AMEX - 15 digits (34,37)
 }
