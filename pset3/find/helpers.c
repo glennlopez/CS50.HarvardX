9b0c4e13e34e8db133d3e116a27fbd3fd735aebb
Glenn Lopez <glennlopez@me.com>
2017-06-14 11:04:30 +0000

    update

---
+++ pset3/find/helpers.c
@@ -0,0 +1,27 @@
+/**
+ * helpers.c
+ *
+ * Helper functions for Problem Set 3.
+ */
+ 
+#include <cs50.h>
+
+#include "helpers.h"
+
+/**
+ * Returns true if value is in array of n values, else false.
+ */
+bool search(int value, int values[], int n)
+{
+    // TODO: implement a searching algorithm
+    return false;
+}
+
+/**
+ * Sorts array of n values.
+ */
+void sort(int values[], int n)
+{
+    // TODO: implement a sorting algorithm
+    return;
+}
