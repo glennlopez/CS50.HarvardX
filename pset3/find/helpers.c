5b37bad1fb4d216df312b2df95c2955659eca36f
Glenn Lopez <glennlopez@me.com>
2017-06-17 02:39:40 +0000

    update

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -13,14 +13,38 @@
  */
 bool search(int value, int values[], int n)
 {
-    // TODO: implement a searching algorithm
-        //return false if n is negative
-        //return fales if value is not in values
-        //return true if value is in values
-        //do not alter function declaration
-        
+    //START BINARY SEARCH ALGORYTHM
+    int midPoint = 0;
+    int startPoint = 0;
+    int endPoint = n -1;
     
-    return false;
+    do{
+        midPoint = ((endPoint + startPoint)/2);
+        if(values[midPoint] != value){
+            
+            if(values[midPoint] < value){
+                startPoint = midPoint + 1;
+            }
+            
+            if(values[midPoint] > value){
+                endPoint = midPoint - 1;
+            }
+            
+            if(startPoint > endPoint){
+                break;
+            }
+        }
+    } 
+    while(values[midPoint] != value);
+    
+    //return results
+    if(startPoint > endPoint){
+        return false;
+    }
+    else{
+        return true;
+    }
+    //END BINARY SEARCH ALGORYTHM
 }
 
 /**
@@ -28,10 +52,26 @@ bool search(int value, int values[], int n)
  */
 void sort(int values[], int n)
 {
-    // TODO: implement a sorting algorithm
-        //sort from smallest to largest
-        //tunning time muse be O(n^2)
-        //do not alter function declaration
-    
+    //START SELECTION SORT ALGORYTHM
+    int smallestNum = values[0];
+    int sortedIndex = 0;
+
+    //Repeat until no unsorted elements remain
+    while(sortedIndex < n){
+        
+        //Search unsorted part of the data to find smallest value
+        for(int i = sortedIndex; i < n ; i++){
+            //Swap the smallest value found with the first element of the unsorted part
+            if(values[i] < smallestNum){
+                smallestNum = values[i];
+                values[i] = values[sortedIndex];
+                values[sortedIndex] = smallestNum;
+            }
+        }
+        
+        sortedIndex++;              //increment sorted index
+        smallestNum = 2147483647;   //set smallestNum largest possible positive int 
+    }
+    //END SELECTION SORT ALGORYTHM
     return;
 }
