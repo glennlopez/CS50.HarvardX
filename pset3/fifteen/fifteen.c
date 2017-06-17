b40c7bd2bdb2f494b57af39ff8e152057d618214
Glenn Lopez <glennlopez@me.com>
2017-06-17 19:45:08 +0000

    started init()

--- pset3/fifteen/fifteen.c
+++ pset3/fifteen/fifteen.c
@@ -3,7 +3,7 @@
  *
  * Implements Game of Fifteen (generalized to d x d).
  *
- * Usage: fifteen d
+ * Usage: fifteen d 
  *
  * whereby the board's dimensions are to be d x d,
  * where d must be in [DIM_MIN,DIM_MAX]
---
+++ sandbox/array.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void init(void);
+
+
+
+
+
+
+//main routine
+int main(){
+    
+    printf("test\n");
+    
+    return 0;
+}
+
+
+
+
+
+//init subroutine
+void init(void){
+    
+    printf("test\n");
+    
+}
