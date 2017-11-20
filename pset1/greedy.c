f794176d320af9d152819d435bea78906e95147e
Glenn Lopez <glennlopez@me.com>
2017-11-20 18:38:14 +0000

    cleaned up code

--- pset1/greedy.c
+++ pset1/greedy.c
@@ -10,7 +10,7 @@
 int main(){
     int coins = 0;
     int cents = 0;
-    float uNum = 0; //NOTE: if you change datatype from float to double see what happens
+    float uNum = 0;
 
     //loop back when uNum is out of bounds
     do{
@@ -19,8 +19,8 @@ int main(){
     }
     while(uNum < 0);
 
-    //converts $1.00 to 100 cents
-    cents = round(uNum * 100);  //<-- round prevents float to int errors
+    //rounds and converts $1.00 to 100 cents
+    cents = round(uNum * 100);
 
     while(cents > 0){
 
@@ -48,28 +48,6 @@ int main(){
             coins++;
             cents -= QUARTER;
         }
-
-        /*  //FAILS
-        if( !(cents % QUARTER) ){
-            coins++;
-            cents -= QUARTER;
-        }
-
-        else if( !(cents % DIME) ){
-            coins++;
-            cents -= DIME;
-        }
-
-        else if( !(cents % NICKEL) ){
-            coins++;
-            cents -= NICKEL;
-        }
-
-        else if( !(cents % PENNY) ){
-            coins++;
-            cents -= PENNY;
-        }
-        */
     }
 
 
