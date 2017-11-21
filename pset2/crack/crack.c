31e12c4bc62ea1a611237fbb99560a6dd18ffd85
Glenn Lopez <glennlopez@me.com>
2017-11-21 03:14:54 +0000

    started crack.c

--- pset2/crack/crack.c
+++ pset2/crack/crack.c
@@ -2,12 +2,16 @@
 #include <stdio.h>
 
 /*
-    [] Accept a single command-line argument (hashed password)
-        [] Print error if no argument or more than one argument is entered
-        [] return 1 on error
-    [] Assume passwords were hashed with DES crypt function
-    [] Assume passwords are no longer than 4 characters
-    [] Assume that each password is composed of alphabetical characters (upper and lower case)
+    [x] Accept a single command-line argument (hashed password)
+        [x] Print error if no argument or more than one argument is entered
+        [x] return 1 on error
+    [1] Assume passwords were hashed with DES crypt function
+    [!] Assume passwords are no longer than 4 characters
+    [!] Assume that each password is composed of alphabetical characters (upper and lower case)
+
+    Topics covered in Week 2:
+        - Functions, Variable scope, Array, Command-line arguments, Magic Numbers
+        - Strings, Ascii, error codes
 */
 
 
@@ -26,13 +30,26 @@ int main(int argc, string argv[]){
         charCnt++;
     }
 
-    //allocate memory for hashed password
+    //allocate memory for hashed password based on character count
     char* hashPwd = malloc(sizeof(char) * charCnt);
+
+    //place argument vector into heap-memory for manipulation
+    for(int i = 0; i < charCnt; i++){
+        hashPwd[i] = argv[1][i];
+    }
+
+
+
+
 
 
     //debug output
     printf("Number of letters: %i \n", charCnt);
-
+    printf("Heap-memory (hashPwd): ");
+    for(int i = 0; i < charCnt; i++){
+        printf("%c", hashPwd[i]);
+    }
+    printf("\n");
 
     printf("\n"); //newline
     return 0;
