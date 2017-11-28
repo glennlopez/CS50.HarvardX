bc5e19436f065ffecc4e6fb736c7c3ca5c0c765c
Glenn Lopez <glennlopez@gmail.com>
2017-11-28 12:21:47 -0600

    update

---
+++ sandbox/week4/swap_pointer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void swap(int*, int*);
+
+int main(){
+
+    int a = 99;
+    int b = 66;
+
+    printf("a: %i\n", a);
+    printf("b: %i\n\n", b);
+
+    swap(&a, &b);
+
+    printf("a: %i\n", a);
+    printf("b: %i\n", b);
+
+    printf("\n");   //new line
+    return 0;
+}
+
+void swap(int *param_a, int *param_b){
+    int temp = *param_a;
+    *param_a = *param_b;
+    *param_b = temp;
+}
