96e236808ec0d94655cda736e9dc57db8c7465af
Glenn Lopez <glennlopez@gmail.com>
2017-12-14 11:28:51 -0600

    working basic fread

--- pset4/shorts/filepointer/fread.c
+++ pset4/shorts/filepointer/fread.c
@@ -3,6 +3,25 @@
 
 int main(){
 
+    //open a file via file pointer
+    FILE *filePointer;
+    char arr[50];
+    size_t sz;
+
+    filePointer = fopen("log_fread.txt", "r");
+    if(filePointer == NULL){
+        perror("filePointer");
+        exit(1);
+    }
+
+    //fread(<buffer>, <size>, <qty>, <filepointer>)
+    sz = fread(arr, sizeof(int), 50, filePointer);
+
+    printf("%s",arr);
+
+    //close file pointer
+    fclose(filePointer);
+
     printf("\n");       //end of main
     return 0;
 }
---
+++ pset4/shorts/filepointer/fread_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <cs50.h>
+int main(){
+
+    
+
+    printf("\n");
+    return 0;
+}
