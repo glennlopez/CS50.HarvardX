f2b88f871170cca8783787a79e8a8e673e6c1cbf
Glenn Lopez <glennlopez@gmail.com>
2017-12-16 10:39:00 -0600

    update

--- pset4/shorts/filepointer/fwrite.c
+++ pset4/shorts/filepointer/fwrite.c
@@ -3,6 +3,21 @@
 
 int main(){
 
+    char *FIXED = "testing...";
+
+    FILE *fp;
+    char *buffer;
+    size_t sz;
+
+    fp = fopen("log_fwrite.txt", "w");
+    buffer = malloc( (sizeof(char) * 50) + 1 );
+
+    for(int i = 0; FIXED[i] != '\0'; i++){
+        buffer[i] = FIXED[i];
+    }
+
+    sz = fwrite(buffer, sizeof(char), 50, fp);
+
     printf("\n");
     return 0;
 }
