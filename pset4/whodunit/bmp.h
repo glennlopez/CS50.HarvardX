bae67d7e9cda61b157d08cc0bfa9acf226645c22
Glenn <glennlopez@gmail.com>
2017-12-20 18:17:43 +0000

    daily update

--- pset4/whodunit/bmp.h
+++ pset4/whodunit/bmp.h
@@ -2,13 +2,13 @@
  * BMP-related data types based on Microsoft's own.
  */
 
-#include <stdint.h> 
+#include <stdint.h>
 
 /**
- * Common Data Types 
+ * Common Data Types
  *
- * The data types in this section are essentially aliases for C/C++ 
- * primitive data types. 
+ * The data types in this section are essentially aliases for C/C++
+ * primitive data types.
  *
  * Adapted from https://msdn.microsoft.com/en-us/library/cc230309.aspx.
  * See http://en.wikipedia.org/wiki/Stdint.h for more on stdint.h.
@@ -26,41 +26,41 @@ typedef uint16_t WORD;
  *
  * Adapted from https://msdn.microsoft.com/en-us/library/dd183374(v=vs.85).aspx.
  */
-typedef struct 
-{ 
-    WORD bfType; 
-    DWORD bfSize; 
-    WORD bfReserved1; 
-    WORD bfReserved2; 
-    DWORD bfOffBits; 
-} __attribute__((__packed__)) 
-BITMAPFILEHEADER; 
+typedef struct
+{
+    WORD bfType;
+    DWORD bfSize;
+    WORD bfReserved1;
+    WORD bfReserved2;
+    DWORD bfOffBits;
+} __attribute__((__packed__))
+BITMAPFILEHEADER;
 
 /**
  * BITMAPINFOHEADER
  *
- * The BITMAPINFOHEADER structure contains information about the 
+ * The BITMAPINFOHEADER structure contains information about the
  * dimensions and color format of a DIB [device-independent bitmap].
  *
  * Adapted from https://msdn.microsoft.com/en-us/library/dd183376(v=vs.85).aspx.
  */
 typedef struct
 {
-    DWORD biSize; 
-    LONG biWidth; 
-    LONG biHeight; 
-    WORD biPlanes; 
-    WORD biBitCount; 
-    DWORD biCompression; 
-    DWORD biSizeImage; 
-    LONG biXPelsPerMeter; 
-    LONG biYPelsPerMeter; 
-    DWORD biClrUsed; 
-    DWORD biClrImportant; 
+    DWORD biSize;
+    LONG biWidth;
+    LONG biHeight;
+    WORD biPlanes;
+    WORD biBitCount;
+    DWORD biCompression;
+    DWORD biSizeImage;
+    LONG biXPelsPerMeter;
+    LONG biYPelsPerMeter;
+    DWORD biClrUsed;
+    DWORD biClrImportant;
 } __attribute__((__packed__))
-BITMAPINFOHEADER; 
+BITMAPINFOHEADER;
 
-/** 
+/**
  * RGBTRIPLE
  *
  * This structure describes a color consisting of relative intensities of
