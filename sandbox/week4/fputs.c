5ddd0dd45c7a2ca3c15db1684e38144ea599bf3d
Glenn <glennlopez@gmail.com>
2017-12-21 20:30:53 +0000

    init practice for fseek

---
+++ sandbox/week4/fputs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(){
+
+    FILE *inputData = fopen("fseek.txt", "w");
+    if(inputData == NULL){
+        printf("Error! File poiunter is null\n");
+        return 1;
+    }
+
+    //fputs puts string, fputc puts char
+    fputs("I ate 55 pumpkins today.", inputData);
+
+    printf("\n");
+    return 0;
+}
