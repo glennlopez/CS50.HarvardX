004c2f82a347513c0f69a91eb12c646a84b03c81
Glenn <glennlopez@gmail.com>
2018-01-14 22:14:37 +0000

    added bubble sort practice primer

--- practice/week_2/bubble_sort/bubble.c
+++ practice/week_2/bubble_sort/bubble.c
@@ -3,6 +3,15 @@
 
  /*
     Bubble sort: https://edx-video.net/HARCS50X2016-V034300_DTH.mp4
+    In bubble sort, the idea is to move higher valued elements at the end of the list
+    and the lower valued elements at the begining of the list (ordered from least to greatest)
+
+    Psuedo code:
+        - Set a swap counter to a non zero value
+        - Repeate until the swap counter is zero
+            - Reset the swap counter to zero
+            - Look at each djacent pair of elements
+                - if two adjacent elements are not in order, swap them and add 1 to the swap counter
 
  */
 
