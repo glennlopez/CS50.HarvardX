25ae31ddfa00fb269fc901ec164dff77f69f725e
Glenn Lopez <glennlopez@gmail.com>
2018-02-21 14:20:46 -0600

    finished factorial practice

--- practice/week_2/recursion/factorial.c
+++ practice/week_2/recursion/factorial.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <cs50.h>
+int getFactorial(int);
 
 int main(){
 
+    int num = get_int("Factorial: ");
+
+    printf("Result: %i", getFactorial(num));
     printf("\n");
     return 0;
+}
+
+/* SUBROUTINE */
+int getFactorial(int n){
+    //base case
+    if(n == 1){
+        return n;
+    }
+
+    //recursive case
+    else{
+        return getFactorial(n - 1) * n;
+    }
 }
