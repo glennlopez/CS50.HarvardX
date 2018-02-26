dc767224d8cd59285f71b2834a618d2c2845fd09
Glenn Lopez <glennlopez@gmail.com>
2018-02-25 18:26:17 -0600

    updates music

---
+++ pset3/2018/music/helpers.c
@@ -0,0 +1,23 @@
+// Helper functions for music
+
+#include <cs50.h>
+
+#include "helpers.h"
+
+// Converts a fraction formatted as X/Y to eighths
+int duration(string fraction)
+{
+    // TODO
+}
+
+// Calculates frequency (in Hz) of a note
+int frequency(string note)
+{
+    // TODO
+}
+
+// Determines whether a string represents a rest
+bool is_rest(string s)
+{
+    // TODO
+}
