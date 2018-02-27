363885df4f424605815616a0158e5283671d1ac1
Glenn Lopez <glennlopez@gmail.com>
2018-02-27 12:15:36 -0600

    finished is_rest

--- pset3/2018/music/helpers.c
+++ pset3/2018/music/helpers.c
@@ -17,7 +17,21 @@ int frequency(string note)
 }
 
 // Determines whether a string represents a rest
-bool is_rest(string s)
-{
-    // TODO
+bool is_rest(string s){
+    
+    //count the number of characters in the string
+    int counter = 0;
+    for(int i = 0; s[i] != '\0'; i++){
+        if(s[i] != '\0'){
+            counter++;
+        }
+    }
+
+    //checks for blank string
+    if(counter <= 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
