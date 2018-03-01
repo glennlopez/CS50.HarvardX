c5e23d80dbab1085923298c324821d946295dce3
Glenn Lopez <glennlopez@gmail.com>
2018-02-28 23:19:42 -0600

    added accidentals

--- pset3/sandbox/frequency.c
+++ pset3/sandbox/frequency.c
@@ -23,37 +23,32 @@ int frequency(string note){
     int strCnt = 0;
     int n_exponent = 0;
     int octive = 0;
+    int octive_pos = 0;
     int octive_frq = 0;
     double note_frq = 0;
     char tone[] = {
         'A','?','B','C','?','D','?','E','F','?','G','?','\0'
     };
 
-    //octive to frequency:
-    octive = (int)note[1] - 48; //calculates octives from 1 to 9
+    //acidental offset adjustment for octive
+    for(strCnt = 0; note[strCnt] != '\0'; strCnt++);
+    if(strCnt == 3){
+        octive_pos = 2;
+    }
+    if(strCnt == 2){
+        octive_pos = 1;
+    }
 
+    //convert octive char to int:
+    octive = (int)note[octive_pos] - 48; 
+
+    //octive to frequency calculator
     if( (note[0] == 'A') || (note[0] == 'B') ){ 
         octive_frq = 440 * ( pow(2,octive - 4) );
     }
     else{ //corrects for octive roll-over
         octive_frq = 440 * ( pow(2,octive - 5) );
     }
-    //FIXME: G4 should be 392, G3 shows up as 392
-    /*
-        C4 - (261.63)   524
-        D4 - (293.66)   588
-        E4 - (329.63)   660
-        F4 - (349.23)   699
-        G4 - (392.00)   784
-        A4 - works      (440.00)
-        B4 - works      (493.88)
-        C5 - (523.25)   1047
-    */
-
-    //count the number of char in the string   
-    for(strCnt = 0; note[strCnt] != '\0'; strCnt++);
-        //if 3 then octive is at note[2]
-        //if 2 then octive is at note[1]
 
     //find exponent(n) for tone calculation 2^(n/12)
     for(n_exponent = 0; tone[n_exponent] != note[0]; n_exponent++){
@@ -63,14 +58,12 @@ int frequency(string note){
         }
     }
 
-    //accidentals to frequency
+    //Return frq:
+    note_frq =  octive_frq * pow(2, (n_exponent/12.00000));
+
+        //accidentals to frequency
         //sharp (#)
         //flats (b)
 
-    //Return frq:
-        // [2 ^ (octive - 4) * (Note Frequency)] +/- accidental frequency = frq
-        note_frq =  octive_frq * pow(2, (n_exponent/12.00000));
-
-    //return rounded number
-    return ceil(note_frq);
+    return ceil(note_frq); //FIXME: E7 (2637.02) is rounded up to 2638 
 }
