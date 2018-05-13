5de7a173afed6dfcae48f31f0f483e331d3e1f30
Glenn <glennlopez@gmail.com>
2018-05-13 22:34:09 +0000

    working resize vertical - no loop

--- pset4/2018/resize/sandbox/resize_x.c
+++ pset4/2018/resize/sandbox/resize_x.c
@@ -113,7 +113,7 @@ int main(int argc, char *argv[])
 
   // DEBUG - HEADER MODIFIERS
     //bi.biWidth = 3;
-    bi.biHeight = 3;
+    bi.biHeight = 6;
     //padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
     //bi.biSizeImage = (((bi.biWidth * sizeof(RGBTRIPLE)) + padding) * abs(bi.biHeight));
     //bf.bfSize = (sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + bi.biSizeImage);
@@ -146,6 +146,37 @@ int main(int argc, char *argv[])
     int index_y = 0;
     for (int i = 0; i < old_biHeight; i++)
     {
+        //for(int scale_y = 0; scale_y < SCALE_FACTOR; scale_y++)
+        //{
+            // write new scanline to output
+            for (int j = 0; j < old_biWidth; j++)
+            {
+                // write each pixel by scale factor
+                for(int scale_x = 0; scale_x < SCALE_FACTOR; scale_x++)
+                {
+                    fwrite(&triple[index_y][index_x], sizeof(RGBTRIPLE), 1, outptr); //fixme
+                }
+                // increment to next pixel
+                index_x++;
+            }
+            // reset index_x to 0
+            index_x = 0;
+        //}
+
+        // increment to the next scanline
+        //index_y++;
+
+        // add padding after scanline
+        for (int k = 0; k < padding; k++) // padding = new padding
+        {
+            fputc(0x00, outptr);
+        }
+
+
+
+
+
+
         // write new scanline to output
         for (int j = 0; j < old_biWidth; j++)
         {
@@ -160,6 +191,10 @@ int main(int argc, char *argv[])
         // reset index_x to 0
         index_x = 0;
 
+
+
+
+
         // increment to the next scanline
         index_y++;
 
@@ -168,6 +203,11 @@ int main(int argc, char *argv[])
         {
             fputc(0x00, outptr);
         }
+
+
+
+
+
     }
 
 
