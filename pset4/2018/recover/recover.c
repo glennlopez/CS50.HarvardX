4d13b59037bef0058b5fbb713a870ae7efd7ff44
Glenn <glennlopez@gmail.com>
2018-05-15 17:35:47 +0000

    added pseudocode

--- pset4/2018/recover/recover.c
+++ pset4/2018/recover/recover.c
@@ -2,12 +2,30 @@
 #include <stdlib.h>
 
 /* TODO:
-    [] open memory card file
+    [x] open memory card file
     [] find begining of jpeg file
+        - first 3 bytes are always the same
+            0xff 0xd8 0xff
+        - forth byte is always 0xE? where "?"" is 0 - F
+        - images are stored contigiously in memory
     [] open a new jpeg
+        - there should be 50 jpeg images
     [] write 512 bytes until new JPEG is found
     [] detect EOF
+*/
 
+/* PSEUDOCODE:
+
+    open card file
+    repeate until end of card
+        read 512 bytes into a buffer
+        Start of a new JPEG?
+            - yes -> ...
+            - no -> ...
+        already found a JPEG?
+            - no -> ...
+            - yes -> ...
+    close any remaining files
 */
 
 int main(int argc, char *argv[])
@@ -22,7 +40,7 @@ int main(int argc, char *argv[])
     // remember file name
     char *rawFile = argv[1];
 
-    // open input file
+    // open memory card file
     FILE *inptr = fopen(rawFile, "r");
     if (inptr == NULL)
     {
