951844512df13145b03079fbd6a84176aac3bf1d
Glenn Lopez <glennlopez@gmail.com>
2021-08-04 21:50:16 +0000

    command line arguement debugging

---
+++ pset2/2021/caesar/caesar.c
@@ -0,0 +1,16 @@
+#include <math.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <cs50.h>
+
+int main(int argc, string argv[])
+{
+
+    // Debug - command line arguments
+    printf("argc: %i\n", argc);
+    for (int i = 0; i < argc; i++)
+    {
+        printf("argv[%i]: %s\n", i, argv[i]);
+    }
+
+}
