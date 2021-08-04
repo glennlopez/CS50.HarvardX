3eb281ea4da100aa6674f8db5b88ad95d1cb9217
Glenn Lopez <glennlopez@gmail.com>
2021-08-04 17:56:04 +0000

    init readability

---
+++ pset2/2021/readability/readability.c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+
+// Globals
+int letter_count;
+
+// Prototypes
+int countletters(string s);
+
+int main(void)
+{
+    // 1. Prompt user for an input
+    string usrInput = get_string("Text: ");
+
+    // 2. Count the number of letters in the string
+
+    // 3. Count the number of words in the string
+
+    // 4. Count the number of sentences in the string
+
+    // 5. Calculate the Coleman-Liau Index
+
+
+    //DEBUG
+    printf("usrInput: %s\n", usrInput);
+
+
+
+}
+
+// Counts the number of letters in a string
+int countletters(string s)
+{
+    letter_count = 0;
+
+
+    return 0;
+}
