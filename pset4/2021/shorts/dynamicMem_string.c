cc2722a57d8265c40c30704465c7019e4f0af956
Glenn Lopez <glennlopez@gmail.com>
2021-09-10 21:11:18 +0000

    practice

--- pset4/2021/shorts/dynamicMem_string.c
+++ pset4/2021/shorts/dynamicMem_string.c
@@ -10,18 +10,33 @@ int main()
 
     //char *t = s;
     char *t = malloc(strlen(s) + 1); // we add + 1 to include "\0" into the copy
+    if(t == NULL)
+    {
+        return 1; //error code 1
+    }
 
     // copy every char to a new string
+    /*
     for(int i = 0; i < strlen(s); i++)
     {
         t[i] = s[i];
     }
+    */
+    strcpy(t, s);
 
     t[0] = toupper(t[0]);
 
-    printf("sizeof(s): %lu\n", strlen(s));
+    printf("strlen(s): %lu\n", strlen(s));
+
+    //free(t);
 
 
     printf("s: %s\n", s);
     printf("t: %s\n", t);
+
+    printf("\n");
+
+    printf("sizeof(t): %lu\n\n", sizeof(t));
+
+    //free(t);
 }
