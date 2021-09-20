de16fce5e6b20c80818896989774029b537f07a3
Glenn Lopez <glennlopez@gmail.com>
2021-09-20 16:42:19 +0000

    code cleanup

--- pset4/2021/filter_less/helpers.c
+++ pset4/2021/filter_less/helpers.c
@@ -20,29 +20,16 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             image[i][j].rgbtBlue = avg;
             image[i][j].rgbtGreen = avg;
             image[i][j].rgbtRed = avg;
-
-            //debug
-            //printf("%x%x%x ", image[i][j].rgbtBlue, image[i][j].rgbtGreen, image[i][j].rgbtRed);
         }
-        //debug
-        //printf("\n");
     }
     return;
 }
 
 // Convert image to sepia
-/*
-  sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
-  sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
-  sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
-
-  note: the resulting value needs to be capped at 255
-*/
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    float sepiaRed = 0;
-    float sepiaGreen = 0;
-    float sepiaBlue = 0;
+    float sepiaRed, sepiaGreen, sepiaBlue;
+
     // itterate through all the pixels
     for(int i = 0; i < height; i++)
     {
@@ -53,7 +40,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             sepiaGreen = (.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
             sepiaBlue = (.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
 
-            // replace pixel tripples with sepia calculated value
+            // replace pixel tripples with sepia calculated value using ternary expression
             image[i][j].rgbtBlue = (sepiaBlue > 255) ? 255 : (int) round(sepiaBlue);
             image[i][j].rgbtGreen = (sepiaGreen > 255) ? 255 : (int) round(sepiaGreen);
             image[i][j].rgbtRed = (sepiaRed > 255) ? 255 : (int) round(sepiaRed);
