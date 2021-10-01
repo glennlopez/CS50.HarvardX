e067336a2db9768c538ccdab3590c28c1ebbb6b6
Glenn Lopez <glennlopez@gmail.com>
2021-10-01 21:27:45 +0000

    cleaned up code, working recovery subroutine

--- pset4/2021/recover/recover.c
+++ pset4/2021/recover/recover.c
@@ -5,11 +5,14 @@
 #include <stdbool.h>
 
 // 0xff 0xd8 0xff (0xe0 - 0xef) = JPEG
-
 int main(int argc, char *argv[])
 {
+    uint8_t buffer[512];
+    int jpeg_counter = 0;
+    char *filename = malloc(sizeof(char) * 30);
+    FILE *jpeg = NULL;
 
-    // open a file pointer for reading
+    // Open a file pointer for reading
     FILE *input = fopen("card.raw", "r");
     if (input == NULL)
     {
@@ -17,62 +20,40 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-
-    uint8_t buffer[512];
-    int jpeg_counter = 0;
-    char *filename = malloc(sizeof(char) * 30);
-    FILE *jpeg = NULL;
-
-
-    // read through the raw data in 512 chunks
+    // Read through the raw data in 512 chunks
     while(fread(&buffer, sizeof(uint8_t), 512, input))
     {
-        // If JPEG is found
+        // If 512 chunk of data in buffer contains JPEG signature
         if ((buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0))
         {
-            //debug
-            printf("JPEG found!\n");
-            printf("[0]: 0x%02x\n", buffer[0]);
-            printf("[1]: 0x%02x\n", buffer[1]);
-            printf("[2]: 0x%02x\n", buffer[2]);
-            printf("[3]: 0x%02x\n", buffer[3]);
-            printf("\n");
-            printf("\n");
-
-            // generate a file name - ###.jpg starting at 000.jpg
+            // Generate a file name for the new jpeg file - ###.jpg starting at 000.jpg
             sprintf(filename, "%03i.jpg", jpeg_counter);
 
-            // open a new file using the filename
+            // Open a new jpeg file pointer
             jpeg = fopen(filename, "w");
 
-            // write the bytes into the file pointer
-            fwrite(&buffer, sizeof(uint8_t), 512, jpeg);
+            // write the 512 chunks of data into the file pointer
+            if(jpeg != NULL)
+            {
+                 fwrite(&buffer, sizeof(uint8_t), 512, jpeg);
+            }
 
+            // Increment the jpeg filename counter for the next jpeg file
             jpeg_counter++;
         }
-        // already found a jpeg
+
+        // If 512 chunk does not contain the signature of a JPEG file
         else
         {
-            // keep writing
+            // keep writing 512 buffer to the jpeg file pointer as long as the file pointer isnt NULL
             if(jpeg != NULL)
             {
                 fwrite(&buffer, sizeof(uint8_t), 512, jpeg);
             }
-
-
         }
 
-
+        // TODO: free memory
 
     }
-
-    printf("Found %i JPEGs in RAW file.\n", jpeg_counter); // debug
-
-
-    printf("\n");
-
-
-    // FREE MEMORY
-    //fclose(input);
 
 }
