379f2a53b00db2f8a4eeb6c9e204b4aa973e5954
Glenn Lopez <glennlopez@gmail.com>
2021-10-19 19:49:34 +0000

    create practice tasks

--- pset5/2021/sandbox/practice_3.c
+++ pset5/2021/sandbox/practice_3.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node_struct
+{
+    int number;
+    struct node_struct *next;
+}
+node;
+
+/* Practice List
+ * [ ] Print a linked list
+ * [ ] Create a new node
+ * [ ] Find a new node
+ * [ ] Insert a node after a value
+ * [ ] Insert a node at a specific pointer
+ * [ ] Delete a node
+ */
+
+
 int main()
 {
 
