a6e420ac22abf329aa32e9698a1a22cc78a18de8
Glenn Lopez <glennlopez@gmail.com>
2021-10-19 20:36:24 +0000

    added create node and print linked list functions

--- pset5/2021/sandbox/practice_3.c
+++ pset5/2021/sandbox/practice_3.c
@@ -10,16 +10,47 @@ node;
 
 /* Practice List
  * [ ] Print a linked list
- * [ ] Create a new node
+ * [x] Create a new node
  * [ ] Find a new node
  * [ ] Insert a node after a value
  * [ ] Insert a node at a specific pointer
  * [ ] Delete a node
  */
 
+void PrintLinkedList(node *head);
+node *CreateNode(int value);
 
 int main()
 {
+    node *head = NULL;
+    node *tmp = NULL;
+
+    for (int i = 1; i <= 5; i++)
+    {
+        tmp = CreateNode(i);
+        tmp->next = head;
+        head = tmp;
+    }
+
+    PrintLinkedList(head);
 
     return 0;
+}
+
+node *CreateNode(int value)
+{
+    node *tmp = malloc(sizeof(node));
+    tmp->number = value;
+    tmp->next = NULL;
+    return tmp;
+}
+
+void PrintLinkedList(node *head)
+{
+    node *tmp = head;
+    while(tmp != NULL)
+    {
+        printf("%i\n", tmp->number);
+        tmp = tmp->next;
+    }
 }
