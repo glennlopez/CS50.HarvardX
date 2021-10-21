6aaea927283fd6462ea9be6ba6dda9809cb70477
Glenn Lopez <glennlopez@gmail.com>
2021-10-21 19:20:01 +0000

    added new practice

---
+++ pset5/2021/sandbox/practice_4.c
@@ -0,0 +1,16 @@
+/* Practice List
+ * [ ] Print a linked list
+ * [ ] Create a new node
+ * [ ] Find a new node
+ * [ ] Insert a node after a value
+ * [ ] Delete a node
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main ()
+{
+
+}
