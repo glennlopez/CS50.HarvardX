e89af5dc27b9e3e0c63fda901e17ec224f1559ea
Glenn Lopez <glennlopez@gmail.com>
2021-11-02 21:15:33 +0000

    practice 9

--- pset5/2021/sandbox/practice_9.c
+++ pset5/2021/sandbox/practice_9.c
@@ -9,8 +9,56 @@ typedef struct node_struct
 }
 node;
 
+void PrintList(node *head);
+node *NewNode(int value);
+
 int main()
 {
+    node *tmp = NULL;
+    node *head = NULL;
 
+    tmp = NewNode(11);
+    tmp->next = head;
+    head = tmp;
 
+    tmp = NewNode(22);
+    tmp->next = head;
+    head = tmp;
+
+    tmp = NewNode(33);
+    tmp->next = head;
+    head = tmp;
+
+    PrintList(head);
+
+}
+
+node *HeadInsertNode(node *new_node)
+{
+    node *updatedList = NULL;
+
+    return updatedList;
+}
+
+// Print linked list
+void PrintList(node *head)
+{
+    node *tmp = head;
+    while (tmp != NULL)
+    {
+        printf("%i\n", tmp->value);
+        tmp = tmp->next;
+    }
+}
+
+// Create a new linked list node
+node *NewNode(int value)
+{
+    node *tmp = malloc(sizeof(node));
+
+    tmp->value = value;
+    tmp->next = NULL;
+    tmp->prev = NULL;
+
+    return tmp;
 }
