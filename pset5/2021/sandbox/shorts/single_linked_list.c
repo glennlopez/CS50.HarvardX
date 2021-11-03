1c46b6a8b8e29faf6144316614c314a19ad1c078
Glenn Lopez <glennlopez@gmail.com>
2021-11-03 21:08:51 +0000

    practice based on short lessons

---
+++ pset5/2021/sandbox/shorts/single_linked_list.c
@@ -0,0 +1,24 @@
+// https://cs50.harvard.edu/x/2021/shorts/singly_linked_lists/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Scope:
+ * [ ] Create a linked list
+ * [ ] Search a linked list
+ * [ ] Insert a new node into a linked list
+ * [ ] Delete a single element from the linked list
+ * [ ] Delete/Free the linked list nodes
+ */
+
+typedef struct sll_node
+{
+    int value;
+    struct sll_node *next;
+}
+node;
+
+int main()
+{
+
+}
