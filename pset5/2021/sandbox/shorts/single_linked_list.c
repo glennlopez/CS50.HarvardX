ca1c33e81691cf3934491e0c215cd1a58fff9107
Glenn Lopez <glennlopez@gmail.com>
2021-11-04 18:44:12 +0000

    working delete func with segfault bugs at tail or head delete

--- pset5/2021/sandbox/shorts/single_linked_list.c
+++ pset5/2021/sandbox/shorts/single_linked_list.c
@@ -8,7 +8,7 @@
  * [x] Search a linked list
  * [x] Insert a new node into a linked list
  * [ ] Delete a single element from the linked list
- * [ ] Delete/Free the linked list nodes
+ * [x] Delete/Free the linked list nodes
  */
 
 typedef struct sll_node
@@ -22,6 +22,8 @@ node *NewNode(int value);
 void PrintList(node *head);
 void Insert(node *new_node, node **head);
 node *Find(int value, node *head);
+void FreeListNodes(node *head);
+void DeleteNode(int target_val, node *head);
 
 int main()
 {
@@ -37,11 +39,64 @@ int main()
     if (find != NULL)
         printf("Number %i, was found!\n", find->value);
 
+    DeleteNode(5, head); //TODO: segfault deleting head or tail
 
+    /* Print the linked list */
     PrintList(head);
+
+    /* Memory Management */
+    FreeListNodes(head);
+
 }
 
-// Search a linked list
+void DeleteNode(int target_val, node *head)
+{
+    node *prevNode = NULL;
+
+    while (head != NULL)
+    {
+        // Set the previous node pointer
+        if ((head->next != NULL) && (head->next->value == target_val))      // IF: next node value has the target value
+            prevNode = head;                                                // point the prevNode pointer to where head is currently pointing
+
+        // Delete the target node
+        if (head->value == target_val)
+        {
+            prevNode->next = head->next; // TODO: fix segfault
+            free(head);
+        }
+
+        head = head->next;
+    }
+
+}
+
+
+/**
+ * @brief  
+ * @note   
+ * @param  *head: 
+ * @retval None
+ */
+void FreeListNodes(node *head)
+{
+    node *tmp = NULL;
+    while (head != NULL)
+    {
+        tmp = head;
+        head = head->next;
+        free(tmp);
+    }
+}
+
+
+/**
+ * @brief  
+ * @note   
+ * @param  value: 
+ * @param  *head: 
+ * @retval 
+ */
 node *Find(int value, node *head)
 {
     for (node *tmp = head; tmp != NULL; tmp = tmp->next)
@@ -50,21 +105,37 @@ node *Find(int value, node *head)
     return NULL;
 }
 
-// Insert a new node in an existing linked list
+/**
+ * @brief  
+ * @note   
+ * @param  *new_node: 
+ * @param  **head: 
+ * @retval None
+ */
 void Insert(node *new_node, node **head)
 {
     new_node->next = *head;
     *head = new_node;
 }
 
-// Print contents for the linked list
+/**
+ * @brief  
+ * @note   
+ * @param  *head: 
+ * @retval None
+ */
 void PrintList(node *head)
 {
     for (node *tmp = head; tmp != NULL; tmp = tmp->next)
         printf("%i\n", tmp->value);
 }
 
-// Malloc a new node in heap memory for a linked list
+/**
+ * @brief  
+ * @note   
+ * @param  value: 
+ * @retval 
+ */
 node *NewNode(int value)
 {
     node *tmp = malloc(sizeof(node));
