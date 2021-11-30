9d3ce35ca71d444b8fc23b3b619cd3c0c2895037
Glenn Lopez <glennlopez@gmail.com>
2021-11-30 18:26:30 +0000

    checkpoint: working node array

--- pset5/2021/sandbox/hashtable/hashtable_intro.c
+++ pset5/2021/sandbox/hashtable/hashtable_intro.c
@@ -2,24 +2,88 @@
 #include <stdlib.h>
 
 #define string char *
+typedef struct node_struct
+{
+    string value;
+    struct node_struct *next;
+}
+node;
 
-int QuickHash(string name, int modval);
+int QuickHash10(string name);
 int StringLen(string s);
+node *NewNode(string strVal);
+void AddToBucket(node **bucket, string value);
 
 int main()
 {
-    int const ALPHA = 26;
-    string uname[3] = {"One", "Two2", "Three3"};
-    int hashTable[ALPHA];
+    // TODO: linked list array
 
-    int a = QuickHash(uname[0], ALPHA);
-    printf("%i\n", a);
-    int b = QuickHash(uname[1], ALPHA);
-    printf("%i\n", b);
-    int c = QuickHash(uname[2], ALPHA);
-    printf("%i\n", c);
+    /* Create empty array */
+    node *tmp[3];
+    for (int i = 0; i < 3; i++)
+        tmp[i] = NULL;
+
+    /* Create new nodes */
+    AddToBucket(&tmp[0], "one_a");
+    AddToBucket(&tmp[0], "one_b");
+    AddToBucket(&tmp[0], "one_c");
+
+    AddToBucket(&tmp[1], "two_a");
+    AddToBucket(&tmp[2], "three_a");
+
+    /* Debug */
+    for (int i = 0; i < 3; i++)
+    {
+        if (tmp[i] != NULL)
+            printf("%s\n", tmp[i]->value);
+    }
+
+    printf("next-> %s\n", tmp[0]->next->value);
+    printf("next-> %s\n", tmp[0]->next->next->value);
+
 
     return 0;
+}
+
+/**
+ * @brief  
+ * @note   
+ * @param  *bucket: Array bucket
+ * @param  value: String value to set the new node
+ * @retval None
+ */
+void AddToBucket(node **bucket, string value)
+{
+    // THIS NEEDS TO BE RECURSIVE AND
+    node *tmp = *bucket;
+
+    /* Base case */
+    if (*bucket == NULL)
+    {
+        *bucket = NewNode(value);
+    }
+
+    /* Recursive case */
+    else
+    {
+        AddToBucket(&tmp->next, value);
+    }
+
+}
+
+
+/**
+ * @brief  Malloc a new node in HEAP memory of size node
+ * @note   
+ * @param  strVal: 
+ * @retval returns a pointer to the newly generated node
+ */
+node *NewNode(string strVal)
+{
+    node *tmp = malloc(sizeof(node));
+    tmp->value = strVal;
+    tmp->next = NULL;
+    return tmp;
 }
 
 /**
@@ -29,7 +93,7 @@ int main()
  * @param  modval: mod value to limit the max value hashed
  * @retval Returns the int hash value
  */
-int QuickHash(string name, int modval)
+int QuickHash10(string name)
 {
     // get string length
     int nameLen = StringLen(name);
@@ -40,7 +104,7 @@ int QuickHash(string name, int modval)
         hashval += name[i];
     }
     
-    return hashval % modval;
+    return hashval % 10;
 }
 
 /**
