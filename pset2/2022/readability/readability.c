ccabcb37008ddf00f19b7d2027337b4394726109
Glenn Lopez <glennlopez@gmail.com>
2022-05-24 19:36:07 +0000

    finished word counter func

--- pset2/2022/readability/readability.c
+++ pset2/2022/readability/readability.c
@@ -2,17 +2,36 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int count_letters(string s);
+int count_letters(string);
+int count_words(string);
 
 int main(void)
 {
     string usrInput = get_string("Text: ");
     int letters = count_letters(usrInput);
+    int words = count_words(usrInput);
 
-    
 
-    printf("counter: %d\n", letters);
 
+
+    printf("counter: %d\n", words);
+
+}
+
+// COunts the number of words in a string
+int count_words(string s)
+{
+    int counter = 0;
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] == ' ')
+        {
+            counter++;
+        }
+    }
+    counter++; // assumes the last space has a word
+
+    return counter;
 }
 
 
