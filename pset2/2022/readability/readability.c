3208cd2edd5ffb4ffeb57ee2364222e5e0178f81
Glenn Lopez <glennlopez@gmail.com>
2022-05-24 19:30:26 +0000

    finished letter counter func

--- pset2/2022/readability/readability.c
+++ pset2/2022/readability/readability.c
@@ -1,7 +1,32 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
 
-int main(void) 
+int count_letters(string s);
+
+int main(void)
 {
+    string usrInput = get_string("Text: ");
+    int letters = count_letters(usrInput);
 
+    
+
+    printf("counter: %d\n", letters);
+
+}
+
+
+// Counts the number of letters in a string
+int count_letters(string s)
+{
+    int counter = 0;
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        if (isalnum(s[i]))
+        {
+            counter++;
+        }
+    }
+
+    return counter;
 }
