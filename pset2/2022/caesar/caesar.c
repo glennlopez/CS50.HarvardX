2a059941eca19934306634504f06d31fe6779692
Glenn Lopez <glennlopez@gmail.com>
2022-05-27 17:50:39 +0000

    finished key

--- pset2/2022/caesar/caesar.c
+++ pset2/2022/caesar/caesar.c
@@ -1,7 +1,55 @@
+//https://cs50.harvard.edu/x/2022/psets/2/caesar/
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int main(int argc, string argv[]) 
+/* Tasks:
+ *  - Get key
+ *  - Get plaintext
+ *  - Encipher
+ *  - Print ciphertext
+*/
+
+int get_key(int, char **);
+bool string_isdigit(string);
+
+
+
+int main(int argc, string argv[])
 {
 
+    // 1. Get Key
+    int key;
+    if (argc == 2 && string_isdigit(argv[1]))
+    {
+        key = atoi(argv[1]);
+    }
+    else
+    {
+        // exit program
+        printf("Usage: ./caesar key\n");
+        return 0;
+    }
+
+    // 2. Get Plaintext
+
+
+
 }
+
+
+// Check is string is all digits
+bool string_isdigit(string s)
+{
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        if (!isdigit(s[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
