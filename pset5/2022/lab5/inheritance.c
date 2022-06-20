c697b52a43d83197b02ff73e1dad599c6cc7333d
Glenn Lopez <glennlopez@gmail.com>
2022-06-20 22:53:53 +0000

    finished STEP 2

--- pset5/2022/lab5/inheritance.c
+++ pset5/2022/lab5/inheritance.c
@@ -79,11 +79,18 @@ person *create_family(int generations)
 // STEP 2. Free `p` and all ancestors of `p`.
 void free_family(person *p)
 {
-    // TODO: Handle base case
+    // TODO: base case - if pointer is null, complete recursion; return to prev call stack
+    if (p == NULL)
+    {
+        return;
+    }
 
     // TODO: Free parents recursively
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
 
     // TODO: Free child
+    free(p);
 
 }
 
