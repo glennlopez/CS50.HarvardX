0de2f4cd2d68839ba8ebac4a89d2d092d228df5c
Glenn Lopez <glennlopez@gmail.com>
2022-06-22 23:20:35 +0000

    working load()

--- pset5/2022/sandbox/linked_list_char.c
+++ pset5/2022/sandbox/linked_list_char.c
@@ -37,7 +37,7 @@ int main ()
     load("large");
 
 
-    PrintList(table[0]);
+    PrintList(table[0]); // debug - watch table[0]
     return 0;
 }
 
@@ -54,59 +54,43 @@ unsigned int hash(const char *word)
 // STEP 1. Loads dictionary into memory, returning true if successful, else false - Load all of the words in the dictionary into datastructure (hashtable)
 bool load(const char *dictionary)
 {
+
     // File pointer params
     const char *filename = dictionary;
     FILE *textfile = fopen(filename, "r");
+    if (textfile == NULL)
+    {
+        return false;
+    }
 
     // Demo - placing words into diffrent buckets in the hashtable
     char buffer[200];
     while (fscanf(textfile, "%s", buffer) != EOF)
     {
-        AddNode(&table[hash(buffer)], buffer);
+
+        // allocate memory in heap
+        node *newNode = NULL;
+        newNode = malloc(sizeof(node));
+
+        // check if enough memory is available
+        if (newNode != NULL)
+        {
+            newNode->word = malloc(strlen(buffer) + 1);
+            strcpy(newNode->word, buffer);
+            newNode->next = NULL;
+        }
+        else
+        {
+            return false;
+        }
+
+        newNode->next = table[hash(buffer)];
+        table[hash(buffer)] = newNode;
     }
-
-
 
     fclose(textfile);
 
-    return false;
-}
-
-
-
-
-
-// Create a new node in heap
-node *NewNode(char* data)
-{
-    // allocate memory in heap
-    node *newNode = NULL;
-    newNode = malloc(sizeof(node));
-
-    // check if enough memory is available
-    if (newNode != NULL)
-    {
-        newNode->word = malloc(strlen(data) + 1);
-        strcpy(newNode->word, data);
-        newNode->next = NULL;
-    }
-    else
-    {
-        free(newNode);
-        printf("Heap overflow! - Failed to allocate node in heap.\n");
-        return 0;
-    }
-
-    return newNode;
-}
-
-
-// Add a node to the list
-void AddNode(node **head, char *word)
-{
-    node *tmp = NewNode(word);
-    tmp->next = *head;
-    *head = tmp;
+    return true;
 }
 
 
