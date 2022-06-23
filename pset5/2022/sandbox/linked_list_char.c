5120069516e66e2ae76cbeacb9ba7255d598222c
Glenn Lopez <glennlopez@gmail.com>
2022-06-23 20:38:34 +0000

    working simple hash

--- pset5/2022/sandbox/linked_list_char.c
+++ pset5/2022/sandbox/linked_list_char.c
@@ -27,7 +27,7 @@ bool load(const char *dictionary);
 unsigned int hash(const char *word);
 
 // Hashtable params
-const unsigned int N = 26; // size of bucket
+const unsigned int N = 330; // size of bucket
 node *table[N];
 
 int main ()
@@ -37,25 +37,33 @@ int main ()
     load("large");
 
 
-    PrintList(table[3]); // debug - watch table[0]
+    PrintList(table[300]); // debug - watch table[0]
     return 0;
 }
 
 
 // STEP 2. Hashes word to a number - Take a word and run a hash function on it, returning some number that coresponds with the word
 unsigned int hash(const char *word)
-{                         //1   2   3   4   5   6   7   8   9   10  1   2   3   4   5   6   7   8   9   20  1   2   3   4   5   6
-    const char alphadex[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-
+{                               //1   2   3   4   5   6   7   8   9   10  1   2   3   4   5   6   7   8   9   20  1   2   3   4   5   6
+    const char alphadex[] = {' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     int indexSum = 0;
 
-    int i = 0;
-    while (alphadex[i] != word[0])
+    int i;
+    for (int j = 0; word[j] != '\0'; j++)
     {
-        i++;
+        i = 0;
+        while (alphadex[i] != word[j])
+        {
+            i++;
+            if (alphadex[i] == word[j])
+            {
+                indexSum += i;
+                continue;
+            }
+        }
     }
 
-    return i;
+    return indexSum % N;
 }
 
 
