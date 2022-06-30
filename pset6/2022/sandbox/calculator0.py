8e4341688e14ac2c9e60f7a7d47f1f2698b7182d
Glenn Lopez <glennlopez@gmail.com>
2022-06-30 21:34:30 +0000

    checkpoint

--- pset6/2022/sandbox/calculator0.py
+++ pset6/2022/sandbox/calculator0.py
@@ -3,7 +3,19 @@ import cs50
 # When importing and not "using" a specific lib
 # programmer needs to add cs50.<library to use> example - cs50.get_int()
 
-x = cs50.get_int("x: ")
-y = cs50.get_int("y: ")
+#x = cs50.get_int("x: ")
+#y = cs50.get_int("y: ")
+
+try:
+    x = int(input("x: "))
+except ValueError:
+    print("value type error!")
+    exit()
+
+try:
+    y = int(input("y: "))
+except ValueError:
+    print("value type error!")
+    exit()
 
 print(f"Sum of x and y: {x + y}");
