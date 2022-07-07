7caeb7d84bd0533d4231c27812c33fc1c4fc0e20
Glenn Lopez <glennlopez@gmail.com>
2022-07-07 17:46:17 +0000

    created a function for meow

--- pset6/2022/sandbox/meow.py
+++ pset6/2022/sandbox/meow.py
@@ -1,6 +1,8 @@
 from cs50 import get_int
 
-usr_input = get_int("How many times to meow: ")
-
-for i in range(usr_input):
+def meow():
     print("Meow.")
+
+usr_input = get_int("How many times to meow: ")
+for i in range(usr_input):
+    meow()
