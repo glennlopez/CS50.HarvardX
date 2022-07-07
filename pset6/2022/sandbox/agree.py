a597e347656cb6932d49224a4cd997c5ebe56e43
Glenn Lopez <glennlopez@gmail.com>
2022-07-07 17:37:48 +0000

    removed reducndant code

--- pset6/2022/sandbox/agree.py
+++ pset6/2022/sandbox/agree.py
@@ -4,9 +4,9 @@ Logical Operators
 from cs50 import get_string
 
 # prompt user to agree (yes or no)
-usr_input = get_string("Do you agree (Y/N): ")
+usr_input = get_string("Do you agree (Y/N): ").lower()[0]
 
-if usr_input.lower()[0] == 'y':
+if usr_input == 'y':
     print("Agreed.")
-elif usr_input.lower()[0] == 'n':
+elif usr_input == 'n':
     print("Disagreed.")
