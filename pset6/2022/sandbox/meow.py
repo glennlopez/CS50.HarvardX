511bc96c3e12fd58570db9372772edc26462c71b
Glenn Lopez <glennlopez@gmail.com>
2022-07-07 17:50:08 +0000

    added main func

--- pset6/2022/sandbox/meow.py
+++ pset6/2022/sandbox/meow.py
@@ -1,8 +1,11 @@
 from cs50 import get_int
 
+def main():
+    usr_input = get_int("How many times to meow: ")
+    for i in range(usr_input):
+        meow()
+
 def meow():
     print("Meow.")
 
-usr_input = get_int("How many times to meow: ")
-for i in range(usr_input):
-    meow()
+main()
