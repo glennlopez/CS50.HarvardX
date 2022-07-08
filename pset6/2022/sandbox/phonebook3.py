d54b5788c8200c3bf2cecc404dfd48f4dd297fe9
Glenn Lopez <glennlopez@gmail.com>
2022-07-08 19:15:47 +0000

    code cleanup

--- pset6/2022/sandbox/phonebook3.py
+++ pset6/2022/sandbox/phonebook3.py
@@ -4,14 +4,14 @@ Dictionaries using CSV files
 from cs50 import get_string
 import csv
 
+# ask user for Name
+name = get_string("Name: ")
+
+# ask user for Number
+num = get_string("Number: ")
+
 # open a file using open(file,a/r/w)
 with open("phonebook.csv", "a") as csv_file:
-
-    # ask user for Name
-    name = get_string("Name: ")
-
-    # ask user for Number
-    num = get_string("Number: ")
 
     # write the name and number to the newly opened csv file
     writer = csv.writer(csv_file)
