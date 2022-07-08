9176182e9427f101fd821fa6e8826f3fbcfa1472
Glenn Lopez <glennlopez@gmail.com>
2022-07-08 19:14:16 +0000

    used with open

--- pset6/2022/sandbox/phonebook3.py
+++ pset6/2022/sandbox/phonebook3.py
@@ -5,17 +5,14 @@ from cs50 import get_string
 import csv
 
 # open a file using open(file,a/r/w)
-csv_file = open("phonebook.csv", "a")
+with open("phonebook.csv", "a") as csv_file:
 
-# ask user for Name
-name = get_string("Name: ")
+    # ask user for Name
+    name = get_string("Name: ")
 
-# ask user for Number
-num = get_string("Number: ")
+    # ask user for Number
+    num = get_string("Number: ")
 
-# write the name and number to the newly opened csv file
-writer = csv.writer(csv_file)
-writer.writerow([name, num])
-
-# close the file
-csv_file.close()
+    # write the name and number to the newly opened csv file
+    writer = csv.writer(csv_file)
+    writer.writerow([name, num])
