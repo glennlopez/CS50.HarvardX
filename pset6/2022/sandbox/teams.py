2cd594424706026d241331100a793aa77c6338c7
Glenn Lopez <glennlopez@gmail.com>
2022-07-12 20:35:38 +0000

    working cmd line argument

--- pset6/2022/sandbox/teams.py
+++ pset6/2022/sandbox/teams.py
@@ -1,6 +1,10 @@
-
+# import required libraries
+import csv
+import sys
 
 # take a command line argument for which file to open
+if len(sys.argv) != 2:
+    sys.exit("Usage: python teams.py <FILENAME>")
 
 # open an dread the file
 
