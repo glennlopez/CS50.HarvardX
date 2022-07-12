732f147415ddc0e29a9da8d1d9b64442cd45a32c
Glenn Lopez <glennlopez@gmail.com>
2022-07-12 20:33:33 +0000

    practice pytho

---
+++ pset6/2022/sandbox/teams.py
@@ -0,0 +1,9 @@
+
+
+# take a command line argument for which file to open
+
+# open an dread the file
+
+# store the teams in the file in a list (teams = [])
+
+# display all the teams stored in teams[]
