737b6d58122378a7181c2b2d67873f1515ce7519
Glenn Lopez <glennlopez@gmail.com>
2022-07-13 21:54:23 +0000

    working - untested

--- pset6/2022/sentimental-mario-less/mario.py
+++ pset6/2022/sentimental-mario-less/mario.py
@@ -16,11 +16,23 @@ def get_height():
 # draw the bricks
 def draw(usr_input):
     width = usr_input
+    blocks = 1
+    # row
     for y in range(usr_input):
+
+        # column - print space
         for x in range(width):
+            print(" ", end="")
+
+        # column - print blocks
+        for block in range(blocks):
             print("#", end="")
+
+        # new line
         print("")
+
         width -= 1
+        blocks += 1
 
 # excecute main if main exists as a def module
 if __name__ == '__main__':
