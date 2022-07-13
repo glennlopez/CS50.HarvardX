04ecb1ad54b96482423d96495a15edd2888e5f3f
Glenn Lopez <glennlopez@gmail.com>
2022-07-13 20:55:43 +0000

    added length and height

--- pset6/2022/sentimental-mario-less/mario.py
+++ pset6/2022/sentimental-mario-less/mario.py
@@ -1,11 +1,10 @@
 # TODO
 from cs50 import get_int
 
+# main
 def main():
-    # length
-    for x in range(get_height()):
-        print("#", end="")
-    print("")
+    usr_input = get_height()
+    draw(usr_input)
 
 
 # prompt the user for Height
@@ -14,5 +13,12 @@ def get_height():
         height = get_int("Height: ")
         if height >= 0:
             return height
+
+# draw the bricks
+def draw(usr_input):
+    for y in range(usr_input):
+        for x in range(usr_input):
+            print("#", end="")
+        print("")
 
 main()
