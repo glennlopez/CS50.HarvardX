26950add6a7b27b1c2589c28019ce439662a54c6
Glenn Lopez <glennlopez@gmail.com>
2022-07-15 17:55:26 +0000

    working readability

--- pset6/2022/sentimental-readability/readability.py
+++ pset6/2022/sentimental-readability/readability.py
@@ -1,14 +1,51 @@
 # libs to import
+from cs50 import get_string
 
+def main():
+    test = get_string("Text: ")
 
+    letter_count = count_letters(test) #debug
+    word_count = count_words(test) #debug
+    sentence_count = count_sentences(test) #debug
+    reading_level = calculate_colemanLaiuIndex(letter_count, word_count, sentence_count)
 
-# Prompt user to type in some text
+    print(f"letter counter: {letter_count}")
+    print(f"word counter: {word_count}")
+    print(f"sentence counter: {sentence_count}")
+    print(f"reading level: {reading_level}")
+
+# coleman laiu index calculator
 def calculate_colemanLaiuIndex(l, w, s):
     index = float(0.0)
     L = float(l / w * 100.0)
     S =  float(s / w * 100.0)
     index = round((0.0588 * L) - (0.296 * S) - 15.8);
-    return index
+    return float(index)
 
+# count letters
+def count_letters(s):
+    counter = 0
+    for char in s:
+        if char.isalpha():
+            counter += 1
+    return counter
 
-# Output the reading grade level
+# count words
+def count_words(s):
+    counter = 0
+    for char in s:
+        if char == " ":
+            counter += 1
+    if counter > 0:
+        counter += 1
+    return counter
+
+def count_sentences(s):
+    counter = 0
+    counter += s.count('!')
+    counter += s.count('.')
+    counter += s.count('?')
+
+    return counter
+
+main()
