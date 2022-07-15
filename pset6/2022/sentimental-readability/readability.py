f57ac85e91c0102480cd65301ef6b85cae3b7003
Glenn Lopez <glennlopez@gmail.com>
2022-07-15 18:04:14 +0000

    finished readability

--- pset6/2022/sentimental-readability/readability.py
+++ pset6/2022/sentimental-readability/readability.py
@@ -1,26 +1,31 @@
 # libs to import
 from cs50 import get_string
 
-def main():
-    test = get_string("Text: ")
 
-    letter_count = count_letters(test) #debug
-    word_count = count_words(test) #debug
-    sentence_count = count_sentences(test) #debug
+def main():
+    usr_text = get_string("Text: ")
+
+    letter_count = count_letters(usr_text)
+    word_count = count_words(usr_text)
+    sentence_count = count_sentences(usr_text)
     reading_level = calculate_colemanLaiuIndex(letter_count, word_count, sentence_count)
 
-    print(f"letter counter: {letter_count}")
-    print(f"word counter: {word_count}")
-    print(f"sentence counter: {sentence_count}")
-    print(f"reading level: {reading_level}")
+    if reading_level < 1:
+        print("Before Grade 1")
+    elif reading_level > 16:
+        print("Grade 16+")
+    else:
+        print(f"Grade {round(reading_level)}")
+
 
 # coleman laiu index calculator
 def calculate_colemanLaiuIndex(l, w, s):
     index = float(0.0)
     L = float(l / w * 100.0)
-    S =  float(s / w * 100.0)
-    index = round((0.0588 * L) - (0.296 * S) - 15.8);
+    S = float(s / w * 100.0)
+    index = round((0.0588 * L) - (0.296 * S) - 15.8)
     return float(index)
+
 
 # count letters
 def count_letters(s):
@@ -29,6 +34,7 @@ def count_letters(s):
         if char.isalpha():
             counter += 1
     return counter
+
 
 # count words
 def count_words(s):
@@ -40,6 +46,8 @@ def count_words(s):
         counter += 1
     return counter
 
+
+# counts sentences
 def count_sentences(s):
     counter = 0
     counter += s.count('!')
@@ -47,5 +55,6 @@ def count_sentences(s):
     counter += s.count('?')
 
     return counter
+
 
 main()
