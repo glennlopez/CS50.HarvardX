cf72bd5cc42b302d30b23b581939454591f06cbe
Glenn Lopez <glennlopez@gmail.com>
2023-03-29 15:15:08 -0500

    completed practice problem

--- pset9/2023/helloflask/app.py
+++ pset9/2023/helloflask/app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "<h3>Functional test</h3>"
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        print("Form submitted!")
+        color = request.form.get("color")
+        return render_template("color.html", color=color)
